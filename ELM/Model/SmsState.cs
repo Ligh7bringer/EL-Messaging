f5a391fa44a82df45877e67ac9efa357ebc185f5
Svetlozar Georgiev <sfe7lio@gmail.com>
2017-11-01 22:05:07 +0000

    starting writing to JSON files (only SMS for now)

--- ELM/Model/SmsState.cs
+++ ELM/Model/SmsState.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace ELM.Model
 {
     class SmsState : MessageState
     {
+        private string Type = "SMS";
+        private string id;
+        private string sender;
+        private string body;
+
+        public string Id { get => id; set => id = value; }
+        public string Sender { get => sender; set => sender = value; }
+        public string Body { get => body; set => body = value; }
+
+
         public SmsState(MessageState state) : this(state.Message)
         {
 
@@ -21,11 +35,23 @@ namespace ELM.Model
         public override void ProcessMessage()
         {
             string id = Message.Header.Substring(1, 9);
-            Console.WriteLine("SMS, id: " + id);
             string sender = Message.Body.Substring(0, 13);
-            Console.WriteLine("sender: " + sender);
-            string message = Message.Body.Substring(13, Message.Body.Length-14);
-            Console.WriteLine("message: " + message);
+            string message = Message.Body.Substring(13, Message.Body.Length-13);
+
+            this.Id = id;
+            this.Sender = sender;
+            this.Body = message;
+
+            JObject data = new JObject(
+                new JProperty("Type", this.Type),
+                new JProperty("ID", this.Id),
+                new JProperty("Sender", this.Sender),
+                new JProperty("Message", this.Body));
+
+            string path = System.Environment.CurrentDirectory + "\\JSON\\" + this.id + ".json";
+            
+            File.WriteAllText(path , data.ToString());
+
 
         }
 
